975400cac4a2f470540f8026a78a4b06020fd43d
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-07-24 11:46:41 +0800

    优化代码

--- src/views/HEXA.vue
+++ src/views/HEXA.vue
@@ -7,16 +7,21 @@
           v-model="item.level"
           :min="item.min"
           :max="item.max"
-          @change="calculate(item.data, item.level, index)"
+          @change="calculate(item.data, item.level, item.target, index)"
+        />
+        <el-text class="mx-1">&nbsp;&nbsp;&nbsp;&nbsp;目标等级：</el-text>
+        <el-input-number
+            v-model="item.target"
+            :min="item.min"
+            :max="item.max"
+            @change="calculate(item.data, item.level, item.target, index)"
         />
       </div>
       <div>
-        <el-text>已消耗碎片数量：</el-text>
-        <el-text>{{ item.cost }}</el-text>
-      </div>
-      <div>
-        <el-text>距离满级还需要：</el-text>
-        <el-text>{{ item.left }}</el-text>
+        <el-text size="small">已消耗碎片数量：</el-text>
+        <el-text size="small">{{ item.cost }}</el-text>
+        <el-text size="small">&nbsp;&nbsp;&nbsp;&nbsp;距离目标还需要：</el-text>
+        <el-text size="small">{{ item.left }}</el-text>
       </div>
     </el-col>
   </el-row>
@@ -26,7 +31,7 @@
         <el-progress
           :percentage="percentage"
           :format="format"
-          style="width: 240px"
+          style="width: 480px"
         />
       </div>
       <div>
@@ -42,7 +47,7 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, computed } from "vue";
+import {reactive, computed, onMounted} from "vue";
 import { ElRow, ElCol, ElText, ElInputNumber, ElProgress } from "element-plus";
 
 const skillCostData = [
@@ -63,6 +68,7 @@ const list = reactive([
     name: "技能核心",
     data: skillCostData,
     level: 1,
+    target: 1,
     cost: 0,
     left: 0,
     min: 1,
@@ -72,6 +78,7 @@ const list = reactive([
     name: "精通1核心",
     data: jingtongCostData,
     level: 0,
+    target: 19,
     cost: 0,
     left: 0,
     min: 0,
@@ -81,6 +88,7 @@ const list = reactive([
     name: "精通2核心",
     data: jingtongCostData,
     level: 0,
+    target: 9,
     cost: 0,
     left: 0,
     min: 0,
@@ -90,6 +98,7 @@ const list = reactive([
     name: "强化1核心",
     data: strongCostData,
     level: 0,
+    target: 1,
     cost: 0,
     left: 0,
     min: 0,
@@ -99,6 +108,7 @@ const list = reactive([
     name: "强化2核心",
     data: strongCostData,
     level: 0,
+    target: 1,
     cost: 0,
     left: 0,
     min: 0,
@@ -108,6 +118,7 @@ const list = reactive([
     name: "强化3核心",
     data: strongCostData,
     level: 0,
+    target: 1,
     cost: 0,
     left: 0,
     min: 0,
@@ -117,6 +128,7 @@ const list = reactive([
     name: "强化4核心",
     data: strongCostData,
     level: 0,
+    target: 0,
     cost: 0,
     left: 0,
     min: 0,
@@ -142,20 +154,38 @@ const percentage = computed(
   () => (totalCost.value / (totalCost.value + totalLeft.value)) * 100 || 0
 );
 
-const calculate = (data: number[], level: number, index: number) => {
+const calculate = (data: number[], level: number, target: number, index: number, save: boolean = true) => {
   let cost = 0;
   let left = 0;
   for (let i = 0; i < level; i++) {
     cost += data[i];
   }
-  for (let i = level; i < data.length; i++) {
+  for (let i = level; i < target; i++) {
     left += data[i];
   }
   list[index].cost = cost;
   list[index].left = left;
+  if (save) {
+    localStorage.setItem("HEXAData", JSON.stringify(list));
+  }
 };
 
 const format = (percentage: number) => percentage.toFixed(2) + "%";
+
+onMounted(() => {
+  const data = localStorage.getItem("HEXAData")
+  if (data) {
+    const items = JSON.parse(data);
+    list.forEach((item) => {
+      const i = items.find((i: any) => i.name === item.name);
+      item.level = i.level;
+      item.target = i.target;
+    });
+  }
+  list.forEach((item, index) => {
+    calculate(item.data, item.level, item.target, index, false);
+  });
+});
 </script>
 
 <style scoped>
