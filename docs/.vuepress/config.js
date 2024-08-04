9ca97964e9184be2fa0d4e364866e7157ea60e4d
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-08-05 04:14:00 +0800

    一些文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,42 @@
-import {viteBundler} from '@vuepress/bundler-vite'
-import {defaultTheme} from '@vuepress/theme-default'
-import {defineUserConfig} from 'vuepress'
+import {viteBundler} from '@vuepress/bundler-vite';
+import {defaultTheme} from '@vuepress/theme-default';
+import {defineUserConfig} from 'vuepress';
+import {mdEnhancePlugin} from "vuepress-plugin-md-enhance";
 
 export default defineUserConfig({
   base: '/docs/',
   dest: 'dist/docs',
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme({
+    sidebarDepth: 4,
+    sidebar: [{
+      text: '游戏玩法',
+      collapsible: true,
+      children: [
+        {
+          text: '术语汇编',
+          collapsible: true,
+          children: [
+            {
+              text: '状态',
+              collapsible: true,
+              children: ['EXP_and_Pet_Closeness', 'Formulas', 'Job_Advancements', 'Traits', 'Inner_Ability', 'Hyper_Stats', 'Maple_Union'],
+            },
+            {
+              text: '装备强化',
+              collapsible: true,
+              children: ['Spell_Trace_and_Star_Force', 'Potential_System', 'Transfer_Hammer', 'Bonus_Stats', 'Soul_Weapon'],
+            },
+          ],
+        },
+        'FAQs',
+      ],
+    }],
+  }),
+  plugins: [
+    mdEnhancePlugin({
+      katex: true,
+      hint: true,
+    }),
+  ],
 })
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,9 @@
 ---
 home: true
 heroText: null
-tagline: '文档正在建设中...敬请期待'
+tagline: '文档正在建设中，有很多很多的内容需要补充'
+actions:
+  - text: 进入文档
+    link: /Spell_Trace_and_Star_Force.html
+    type: primary
 ---
