3965e128286b0ee991083736111d21f8ca7da127
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-08-05 19:28:51 +0800

    优化文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,29 +9,17 @@ export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme({
     sidebarDepth: 4,
-    sidebar: [{
-      text: '游戏玩法',
-      collapsible: true,
-      children: [
-        {
-          text: '术语汇编',
-          collapsible: true,
-          children: [
-            {
-              text: '状态',
-              collapsible: true,
-              children: ['EXP_and_Pet_Closeness', 'Formulas', 'Job_Advancements', 'Traits', 'Inner_Ability', 'Hyper_Stats', 'Maple_Union'],
-            },
-            {
-              text: '装备强化',
-              collapsible: true,
-              children: ['Spell_Trace_and_Star_Force', 'Potential_System', 'Transfer_Hammer', 'Bonus_Stats', 'Soul_Weapon'],
-            },
-          ],
-        },
-        'FAQs',
-      ],
-    }],
+    navbar: [
+      {
+        text: '角色属性',
+        children: ['EXP_and_Pet_Closeness', 'Formulas', 'Job_Advancements', 'Traits', 'Inner_Ability', 'Hyper_Stats', 'Maple_Union'],
+      },
+      {
+        text: '装备强化',
+        children: ['Spell_Trace_and_Star_Force', 'Potential_System', 'Transfer_Hammer', 'Bonus_Stats', 'Soul_Weapon'],
+      },
+      'FAQs',
+    ],
   }),
   plugins: [
     mdEnhancePlugin({
