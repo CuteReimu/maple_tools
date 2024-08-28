0ab1dff5a24542054fada3086dd28ea2d1186717
Death <546115696@qq.com>
2024-08-28 17:12:12 +0800

    解决打开页面闪烁

--- src/views/LegionResolver.vue
+++ src/views/LegionResolver.vue
@@ -1,9 +1,26 @@
 <template>
-  <iframe :src="legionUrl" height="1024px" width="100%" class="iframe"></iframe>
+  <iframe
+    v-show="loaded"
+    ref="iframeRef"
+    :src="legionUrl"
+    height="1024px"
+    width="100%"
+    class="iframe"
+  />
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
 const legionUrl = import.meta.env.VITE_LEGION_URL;
+const iframeRef = ref();
+const loaded = ref(false);
+
+onMounted(() => {
+  iframeRef.value.onload = () => {
+    loaded.value = true;
+  };
+});
 </script>
 
 <style scoped>
