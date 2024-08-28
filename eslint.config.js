b96479a50d70259a2fada41963e109a9578759a4
Death <546115696@qq.com>
2024-08-28 11:18:03 +0800

    添加拖拽功能（需实现后续逻辑）

--- eslint.config.js
+++ eslint.config.js
@@ -5,13 +5,13 @@ import pluginVue from "eslint-plugin-vue";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: {globals: globals.browser}},
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/recommended"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
-  {ignores: ["dist/", "node_modules/"]},
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  { ignores: ["dist/", "node_modules/"] },
   {
     rules: {
       "indent": ["error", 2],
@@ -19,8 +19,9 @@ export default [
       "vue/multiline-html-element-content-newline": "off",
       "vue/max-attributes-per-line": [
         "warn",
-        {"singleline": {"max": 3}, "multiline": {"max": 1}},
+        { "singleline": { "max": 3 }, "multiline": { "max": 1 } },
       ],
+      "@typescript-eslint/no-unused-vars": "off"
     },
   },
 ];
