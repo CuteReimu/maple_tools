f7e7dbca0253f4b65c2fa2c6551244f8ae74c99b
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-04-02 17:07:37 +0800

    洗魔方概率表

--- src/.vuepress/components/PotentialSystem.vue
+++ src/.vuepress/components/PotentialSystem.vue
@@ -53,15 +53,83 @@
       {{ item }}
     </el-text>
   </div>
+  <h2>属性概率表(%)</h2>
+  <el-table :data="tableData" border>
+    <el-table-column
+      prop="type"
+      class-name="overflow-hidden"
+      show-overflow-tooltip
+      label="属性"
+      width="230"
+    />
+    <el-table-column prop="first_line" label="第一条" width="90" />
+    <el-table-column prop="second_line" label="第二条" width="90" />
+    <el-table-column prop="third_line" label="第三条" width="90" />
+  </el-table>
 </template>
 
 <script setup lang="ts">
 import {computed, reactive, ref} from "vue";
 import {
   ElText, ElSelect, ElOption, ElRadioGroup, ElRadioButton,
-  ElForm, ElFormItem, ElButton,
+  ElForm, ElFormItem, ElButton, ElTable, ElTableColumn,
 } from "element-plus";
 import { cubeRates } from "./cubeRates.js";
+
+const importantAttr = ["STR", "DEX", "INT", "LUK", "All Stats", "Critical Damage", "Item Drop Rate", "Meso Amount"];
+const tableData = computed(() => {
+  const result = [];
+  const rate = cubeRates.lvl120to200[form.position][form.type].legendary;
+  const rates = [rate.first_line, rate.second_line, rate.third_line];
+  for (let i = 0; i < rates.length; i++) {
+    for (const line of rates[i]) {
+      let lineType = line[0] as string;
+      const lineValue = line[1] as number | string | string[];
+      const lineRate = line[2] as number;
+      if (lineType === "Junk") {
+        lineType = "其它垃圾属性";
+      } else if (lineType.includes("Flat")) {
+        lineType = lineType.replace("Flat", `+${lineValue}`);
+      } else if (lineType.includes("%")) {
+        lineType = lineType.replace("%", lineValue + "%");
+      } else if (lineType === "Boss Damage") {
+        lineType = `${lineType} ${lineValue}%`;
+      } else {
+        lineType = `${lineType}: ${lineValue}%`;
+      }
+      let v = result.find((value) => value.type === lineType);
+      if (!v) {
+        v = {type: lineType, first_line: 0, second_line: 0, third_line: 0};
+        result.push(v);
+      }
+      if (i === 0) v.first_line = lineRate;
+      else if (i === 1) v.second_line = lineRate;
+      else v.third_line = lineRate;
+    }
+  }
+  return result.sort((a, b) => {
+    if (a.type === "其它垃圾属性" && b.type !== "其它垃圾属性") return 1;
+    if (a.type !== "其它垃圾属性" && b.type === "其它垃圾属性") return -1;
+    if (["weapon", "secondary", "emblem"].includes(form.position)) {
+      const isImportant = (t: string) =>
+        t.includes("ATT") && t.includes("%") || t.includes("Boss Damage") || t.includes("Ignore Enemy Defense");
+      const iA = isImportant(a.type);
+      const iB = isImportant(b.type);
+      if (iA != iB) return iA ? -1 : 1;
+    } else {
+      const isImportant = (t: string) =>
+        importantAttr.some((s) => t.includes(s)) && t.includes("%") || t.includes("Skill Cooldown Reduction");
+      const iA = isImportant(a.type);
+      const iB = isImportant(b.type);
+      if (iA != iB) return iA ? -1 : 1;
+      else if (iA && iB) {
+        if (a.type.length >= 15 && b.type.length < 15) return -1;
+        if (a.type.length < 15 && b.type.length >= 15) return 1;
+      }
+    }
+    return a.type.length - b.type.length;
+  });
+});
 
 const form = reactive({
   type: "red",
@@ -133,5 +201,19 @@ const doReset = () => {
 </script>
 
 <style scoped>
+:deep(.el-table__header-wrapper .el-table__header) {
+  margin: 0;
+}
 
+:deep(.el-table__body-wrapper .el-table__body) {
+  margin: 0;
+}
+
+:deep(th) {
+  border: none;
+}
+
+:deep(td) {
+  border: none;
+}
 </style>
--- src/Tools/PotentialSystem.md
+++ src/Tools/PotentialSystem.md
@@ -5,11 +5,11 @@ icon: cube
 order: 6
 ---
 
-<PotentialSystem></PotentialSystem>
-
 ::: note 注意
 每次洗潜能，除了魔方消耗以外，还有一个和装备等级相关的Mesos消耗。由于该消耗占比较低，本计算器将其忽略，只计算魔方在无活动情况下的购买消耗。
 :::
+
+<PotentialSystem></PotentialSystem>
 
 <script setup>
 import PotentialSystem from "@PotentialSystem";
