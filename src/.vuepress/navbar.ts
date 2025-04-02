98b881c934db013bbba3552a98bec84a98ae71e5
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-04-02 18:33:34 +0800

    优化navbar

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,18 +3,24 @@ export default navbar([
   '/Tools/',
   '/Dictionary/',
   {
-    text: "NGA论坛",
-    link: "https://ngabbs.com/thread.php?fid=707&rand=932",
-    icon: "/images/nga.png",
-  },
-  {
-    text: "BiliWiki",
-    link: "https://wiki.biligame.com/maplestory",
-    icon: "b:bilibili",
-  },
-  {
-    text: "猫久攻略",
-    link: "https://bbs.gjfmxd.com/",
-    icon: "/images/maplestory.png",
-  },
+    text: "常用链接",
+    icon: "link",
+    children: [
+      {
+        text: "NGA论坛",
+        link: "https://ngabbs.com/thread.php?fid=707&rand=932",
+        icon: "/images/nga.png",
+      },
+      {
+        text: "BiliWiki",
+        link: "https://wiki.biligame.com/maplestory",
+        icon: "b:bilibili",
+      },
+      {
+        text: "猫久攻略",
+        link: "https://bbs.gjfmxd.com/",
+        icon: "/images/maplestory.png",
+      },
+    ],
+  }
 ]);
