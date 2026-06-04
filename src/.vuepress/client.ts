617a8f3e3b3d4658db79a42c3dae98c3727498dc
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-06-04 23:01:35 +0800

    Fix compile error

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,9 +1,11 @@
 import {defineClientConfig} from 'vuepress/client';
 import 'element-plus/dist/index.css'
 import "element-plus/theme-chalk/dark/css-vars.css";
+import { ID_INJECTION_KEY } from 'element-plus';
 
 export default defineClientConfig({
-  enhance() {
+  enhance({ app }) {
+    app.provide(ID_INJECTION_KEY, { prefix: Math.floor(Math.random() * 100000000), current: 0 });
     if (typeof window !== 'undefined' && typeof document !== 'undefined') {
       // 监听 html 元素的 data-theme 属性变化
       const observer = new MutationObserver((mutations) => {
