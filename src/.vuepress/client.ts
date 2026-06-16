cb34efff40a68116681cada8deb5feeee5243463
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-06-16 11:19:14 +0800

    Bump deps

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,11 +1,12 @@
 import {defineClientConfig} from 'vuepress/client';
 import 'element-plus/dist/index.css'
 import "element-plus/theme-chalk/dark/css-vars.css";
-import { ID_INJECTION_KEY } from 'element-plus';
+import { ID_INJECTION_KEY, ZINDEX_INJECTION_KEY } from 'element-plus';
 
 export default defineClientConfig({
   enhance({ app }) {
     app.provide(ID_INJECTION_KEY, { prefix: Math.floor(Math.random() * 100000000), current: 0 });
+    app.provide(ZINDEX_INJECTION_KEY, { current: 0 });
     if (typeof window !== 'undefined' && typeof document !== 'undefined') {
       // 监听 html 元素的 data-theme 属性变化
       const observer = new MutationObserver((mutations) => {
